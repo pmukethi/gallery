453a47b595f4fceca8152b2bf6fa35e73abbb29d
pmukethi <pamela.mukethi8@gmail.com>
2023-06-26 21:02:21 +0300

    updated the DB passwords

--- _config.js
+++ _config.js
@@ -2,7 +2,7 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://pamelamukethi8:excell@2023@cluster0.o7vm83f.mongodb.net/darkroom?retryWrites=true&w=majority',
+    production: 'mongodb+srv://pamelamukethi8:excell@20231@cluster0.o7vm83f.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://pamelamukethi8:excell@2023@cluster0.o7vm83f.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     test: 'mongodb+srv://pamelamukethi8:excell@2023@cluster0.o7vm83f.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
