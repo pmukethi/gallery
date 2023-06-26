2b21c1ece93e7a77466ff82d6fac5fa1c2f8c691
pmukethi <pamela.mukethi8@gmail.com>
2023-06-26 19:36:43 +0300

    updated the DB username and password to connect to mongo DB

--- _config.js
+++ _config.js
@@ -2,7 +2,7 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    production: 'mongodb+srv://pamelamukethi8:excell@2023@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
