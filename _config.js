3ef102b6908a872d9a78291d665838c5c01e52c5
pmukethi <pamela.mukethi8@gmail.com>
2023-06-26 20:43:05 +0300

    updated the DB connection

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://pamelamukethi8:excell@2023@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://pamelamukethi8:excell@2023@cluster0.o7vm83f.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://pamelamukethi8:excell@2023@cluster0.o7vm83f.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://pamelamukethi8:excell@2023@cluster0.o7vm83f.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
